9fb144ccdf3d9f3e03b4486c1d24fce7c45f0171
cchurtado18 <cchurtado@uamv.edu.ni>
2026-03-05 13:41:45 -0600

    Forzar zona horaria Miami al arranque para guardar y mostrar hora correcta

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Preregistration;
 use App\Observers\PreregistrationObserver;
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,9 +19,15 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
+     * Fijamos la zona horaria de Miami para que al guardar paquetes (created_at, etc.)
+     * y al mostrar fechas se use siempre hora de Miami.
      */
     public function boot(): void
     {
+        $tz = config('app.timezone', 'America/New_York');
+        date_default_timezone_set($tz);
+        Carbon::setDefaultTimezone($tz);
+
         Preregistration::observe(PreregistrationObserver::class);
     }
 }
